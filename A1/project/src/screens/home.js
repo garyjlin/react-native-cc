f96dc7bd9d5f265110321135f541f3618043e36d
ohshizournizz <jamiejiahao@gmail.com>
2018-02-02 22:39:05 -0500

    partial changes to home.js

--- A1/project/src/screens/home.js
+++ A1/project/src/screens/home.js
@@ -8,9 +8,11 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native';
+import Tabs from 'react-native-tabs';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -21,30 +23,32 @@ const instructions = Platform.select({
 
 export default class Home extends Component<{}> {
   render() {
+	 // const { page } = this.state;
+  //const background = styles[page];
+ // const tabbarStyles = [styles.tabbar];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          HOME asdfasd
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit Home.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View>
+	  <Text style={styles.welcome}>
+          $USD: 123
+      </Text>
+      <Text style={styles.instructions}>
+          View your account 
+      </Text>
+   <Image
+          style={{width: 50, height: 50}}
+          source={{uri: 'http://www.marketoracle.co.uk/images/2013/Feb/Microsoft_share_price_chart.jpg'}}
+        />
+	  <View style={{width: 400, height: 300, backgroundColor: 'steelblue'}} />
+
+		
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   welcome: {
+	marginTop: 5,
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
@@ -52,6 +56,12 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 3,
   },
+    tabbar: {
+    backgroundColor:'white',
+    height: 64,
+    borderTopColor: 'red',
+    borderTopWidth: 2
+  }
 });
